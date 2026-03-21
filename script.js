74a3ec324f29ed592294ee54bbbb1df157874bea
krokodil22 <vkruzhalov@bk.ru>
2026-03-21 17:36:32 +0500

    Show Blockly command blocks in left flyout

--- script.js
+++ script.js
@@ -35,7 +35,31 @@ const levelTitle = document.getElementById('level-title');
 const levelDescription = document.getElementById('level-description');
 const levelProgress = document.getElementById('level-progress');
 const workspaceContainer = document.getElementById('blockly-workspace');
-const toolbox = document.getElementById('blockly-toolbox');
+
+const toolbox = {
+  kind: 'flyoutToolbox',
+  contents: [
+    {
+      kind: 'block',
+      type: 'maze_move_forward',
+    },
+    {
+      kind: 'block',
+      type: 'maze_turn_left',
+    },
+    {
+      kind: 'block',
+      type: 'maze_turn_right',
+    },
+    {
+      kind: 'block',
+      type: 'maze_repeat',
+      fields: {
+        TIMES: 2,
+      },
+    },
+  ],
+};
 
 let workspace;
 let currentLevelIndex = 0;
@@ -99,6 +123,8 @@ Blockly.common.defineBlocksWithJsonArray([
 function initializeBlockly() {
   workspace = Blockly.inject(workspaceContainer, {
     toolbox,
+    toolboxPosition: 'start',
+    horizontalLayout: false,
     trashcan: true,
     grid: {
       spacing: 24,
@@ -123,6 +149,7 @@ function initializeBlockly() {
   });
 
   resetWorkspace();
+  requestAnimationFrame(() => Blockly.svgResize(workspace));
   window.addEventListener('resize', () => Blockly.svgResize(workspace));
 }
 
@@ -132,6 +159,7 @@ function resetWorkspace() {
   startBlock.initSvg();
   startBlock.render();
   startBlock.moveBy(36, 36);
+  Blockly.svgResize(workspace);
 }
 
 function toKey([row, col]) {
